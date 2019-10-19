d28435d0fc5844082c0d73cd044f3ae3f458f2aa
treyanarose <treyanarose@gmail.com>
2019-10-19 18:57:56 +0630

    UI

---
+++ src/main/java/com/example/doctor/appointment/controller/HomeController.java
@@ -0,0 +1,48 @@
+package com.example.doctor.appointment.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.doctor.appointment.entity.Department;
+import com.example.doctor.appointment.entity.Doctor;
+import com.example.doctor.appointment.entity.Schedule;
+import com.example.doctor.appointment.service.DepartmentService;
+import com.example.doctor.appointment.service.DoctorService;
+import com.example.doctor.appointment.service.ScheduleService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private DoctorService doctorService;
+	
+	@Autowired
+	private ScheduleService scheduleService;
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	
+	@GetMapping("/docPf")
+	public String showSchedules(Model model) {
+		List<Schedule> Schedules=scheduleService.getSchedules();
+		List<Doctor> doctors=doctorService.getDoctors();
+		model.addAttribute("doctor",doctors);
+		model.addAttribute("schedules",Schedules);
+		return "doctor_profile";
+	}
+	
+	@GetMapping("/")
+	public String showDepartments(Model model) {
+		
+		List<Department> department=departmentService.getDepartments();
+		model.addAttribute("departments",department);
+		return "home";
+	}
+	
+	
+}
